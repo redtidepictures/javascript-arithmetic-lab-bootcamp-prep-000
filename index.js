e88f320a5c975983e679f5b903b72e72ababe8a6
Tyler Wales <walestyl@gmail.com>
2017-10-04 08:03:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n + 1;
+  return n += 1
 }
 
 function dec(n) {
-  return n - 1;
+  return n -= 1
 }
 
-function makeInt('n') {
-  return n;
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
